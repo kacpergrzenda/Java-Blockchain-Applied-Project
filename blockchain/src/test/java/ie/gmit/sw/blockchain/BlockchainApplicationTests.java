f138fed9ed75fea64a607502293f754c0f0ce4a6
unknown <g00373427@gmit.ie>
2022-01-25 21:05:21 +0000

    Initialization of Spring

---
+++ blockchain/src/test/java/ie/gmit/sw/blockchain/BlockchainApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.gmit.sw.blockchain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockchainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
